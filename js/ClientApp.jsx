949d45fdbd9ab6aba9c02a9599ca392d15212dbc
Shahid Khan <shahid.manit@gmail.com>
2018-04-06 22:56:36 -0500

    images and card on search page
    images and card on search page

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter, Route, Switch} from 'react-router-dom'
-import Landing from './Landing'
-import Search from './Search'
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Landing from './Landing';
+import Search from './Search';
 
-const FourOFour = () => <h1>404</h1>
+const FourOFour = () => <h1>404</h1>;
 
 const App = () => (
   <BrowserRouter>
-    <div className='app'> 
+    <div className="app">
       <Switch>
-      <Route exact path="/" component={Landing} />
-      <Route exact path="/search" component={Search} />
-      <Route component={FourOFour} />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/search" component={Search} />
+        <Route component={FourOFour} />
       </Switch>
     </div>
   </BrowserRouter>
-  );
+);
 
 render(<App />, document.getElementById('app'));
