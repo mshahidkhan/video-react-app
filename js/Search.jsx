b8ccfe6d8ac7523be86a565fca6d6e5d211109af
Shahid Khan <shahid.manit@gmail.com>
2018-04-08 21:08:08 -0500

    ES6 Class transformation
    ES6 Class transformation

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ShowCard from './ShowCard'
 import preload from '../data.json';
 
-const Search = () => (
-  <div className="search">
-    <div>
-      <header>
-        <h1>MSK Video</h1>
-        <input type="text" placeholder="Search"/>
-      </header>
+class Search extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchTerm: 'Some search string'
+    };
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this)
+  }
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+    
+  }
+  render () {
+    return ( 
+      <div className="search">
+      <div>
+        <header>
+          <h1>MSK Video</h1>
+          <input 
+            onChange={this.handleSearchTermChange} 
+            value={this.state.searchTerm} 
+            type="text" 
+            placeholder="Search"/>
+        </header>
+      </div>
+      <div>
+        {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} /> )}
+      </div>
     </div>
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} /> )}
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Search;
