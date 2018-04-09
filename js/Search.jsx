a93a626761e3998052aa580190ed0838db7485f7
Shahid Khan <shahid.manit@gmail.com>
2018-04-08 20:50:08 -0500

    heading
    heading

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,6 +5,12 @@ import preload from '../data.json';
 const Search = () => (
   <div className="search">
     <div>
+      <header>
+        <h1>MSK Video</h1>
+        <input type="text" placeholder="Search"/>
+      </header>
+    </div>
+    <div>
       {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} /> )}
     </div>
   </div>
