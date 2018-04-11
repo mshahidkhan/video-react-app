6b9e20382569cd876d29f41f3c4e8730ac0cab77
Shahid Khan <shahid.manit@gmail.com>
2018-04-11 16:53:14 -0500

    test coverage

--- js/__test__/Search.spec.jsx
+++ js/__test__/Search.spec.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Search from '../Search'
+import preload from '../../data.json';
+import Search from '../Search';
+import ShowCard from '../ShowCard';
+
+// Do with describe
+//
+// describe('Search', () => {
+//   it('should Search renders correctly', () => {
+//     const component = shallow(<Search />)
+//     expect(component).toMatchSnapshot();
+//   }) 
+// })
 
 test('Search renders correctly', () => {
   const component = shallow(<Search />)
   expect(component).toMatchSnapshot();
 }) 
+
+test('Search should render correct amount of shows', () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+})
+
+test('Search should renders correct amount of shows based on Search term', () => {
+  const searchWord = 'black';
+  const component = shallow(<Search />);
+  component.find('input').simulate('change', { target: { value: searchWord } });
+  const showCount = preload.shows.filter( show => 
+    `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0 ).length;
+  expect(component.find(ShowCard).length).toEqual(showCount);
+});
